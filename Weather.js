f5b7fa4b5896a60bb21ebb305262277885b4b8a1
Noah <savedream75@gmail.com>
2019-05-27 11:45:15 +0900

    Prettier Weather.js

--- Weather.js
+++ Weather.js
@@ -5,76 +5,85 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
 const weatherCases = {
   Rain: {
-    colors:['#00C6FB', "#005BEA"],
+    colors: ["#00C6FB", "#005BEA"],
     title: "Raining like a MF",
     subtitle: "For more info look outside",
     icon: "weather-rainy"
   },
   Clear: {
-    colors:['#FEF253', "#FF7300"],
+    colors: ["#FEF253", "#FF7300"],
     title: "Sunny",
     subtitle: "Go to the outside",
     icon: "weather-sunny"
   },
   Thunderstrorm: {
-    colors:['#00ECBC', "#007ADF"],
+    colors: ["#00ECBC", "#007ADF"],
     title: "Thunderstorm in the house",
     subtitle: "Actually, outside of the house",
     icon: "weather-lightning"
   },
   Clouds: {
-    colors:['#D7D2CC', "#304352"],
+    colors: ["#D7D2CC", "#304352"],
     title: "Coluds",
     subtitle: "I know, It's boring",
     icon: "weather-cloudy"
   },
   Snow: {
-    colors:['#7DE2FC', "#B9B6E5"],
+    colors: ["#7DE2FC", "#B9B6E5"],
     title: "Cold as balls",
     subtitle: "Do you want to build a snowman?",
     icon: "weather-snowy"
   },
   Drizzle: {
-    colors:['#89F7FE', "#66A6FF"],
+    colors: ["#89F7FE", "#66A6FF"],
     title: "Drizzle",
     subtitle: "Is like rain",
     icon: "weather-hail"
   },
   Haze: {
-    colors:['#89F7FE', "#66A6FF"],
+    colors: ["#89F7FE", "#66A6FF"],
     title: "Haze",
     subtitle: "Be careful",
     icon: "weather-fog"
   },
   Mist: {
-    colors:['#D7D2CC', "#304352"],
+    colors: ["#D7D2CC", "#304352"],
     title: "Mist",
     subtitle: "It's like yo have no glasses on.",
     icon: "weather-fog"
-  },
-}
+  }
+};
 
 const Weather = ({ temp, weatherName }) => {
   return (
-    <LinearGradient style={styles.container} colors={weatherCases[weatherName].colors}>
+    <LinearGradient
+      style={styles.container}
+      colors={weatherCases[weatherName].colors}
+    >
       <View style={styles.upper}>
         <Text style={styles.icon}>
-          <MaterialCommunityIcons color="white" size={144} name={weatherCases[weatherName].icon} />
+          <MaterialCommunityIcons
+            color="white"
+            size={144}
+            name={weatherCases[weatherName].icon}
+          />
         </Text>
         <Text style={styles.temp}>{temp}º</Text>
       </View>
       <View style={styles.lower}>
         <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
-        <Text style={styles.subtitle}>{weatherCases[weatherName].subtitle}</Text>
+        <Text style={styles.subtitle}>
+          {weatherCases[weatherName].subtitle}
+        </Text>
       </View>
     </LinearGradient>
   );
-}
+};
 
 Weather.propTypes = {
   temp: PropTypes.number.isRequired,
   weatherName: PropTypes.string.isRequired
-}
+};
 
 export default Weather;
 
